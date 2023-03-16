6c18ef87a1568c7f33dd943088f3f29497da6324
Snooopyman <armando.snoop@gmail.com>
2023-03-16 15:52:53 +0100

    Initial Commit

---
+++ MyFirstApp/MyFirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstAppApp.swift
+//  MyFirstApp
+//
+//  Created by Armando Cáceres on 16/3/23.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
